21e38434e97aa7d5b5b248675fffedcf3eb9d504
Raffay Syed <raffayhusayn@gmail.com>
2021-12-05 23:04:59 -0500

    Added a movie class so that instead of returning a list of String, I can return a movie object

---
+++ src/main/java/com/fclass/MovieScrapper/Movie.java
@@ -0,0 +1,36 @@
+package com.fclass.MovieScrapper;
+
+public class Movie {
+    String name;
+    int releaseYear;
+    int rank;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return name + "( "+ releaseYear + " )";
+    }
+}
