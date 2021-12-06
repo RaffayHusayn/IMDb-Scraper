e31ea3da86ca288a313a26a58e978f5dc186526f
Raffay Syed <raffayhusayn@gmail.com>
2021-12-06 04:21:16 -0500

    Some comments to make the code more understandable

--- src/main/java/com/fclass/App.java
+++ src/main/java/com/fclass/App.java
@@ -12,26 +12,24 @@ public class App
 {
     public static void main( String[] args ) throws IOException {
 
+        /*
+        This returns a Hashmap<Integer, Movie>
+         */
         ImdbUserListScrapper movieList = new ImdbUserListScrapper();
         movieList.scrapImdbUserList("https://www.imdb.com/list/ls098795660/?ref_=tt_rls_1");
-//
+
+        /*
+        This returns Hashmap<Integer, List<String>>
+         */
 //        ImdbScrapper top50 = new ImdbScrapper();
 //        top50.top50List();
-////
-//        List<String> directorList = new ArrayList<>();
-//        List<String> castList = new ArrayList<>();
-//        StringCleaning sc = new StringCleaning();
-//        sc.directorAndCastSplitor("Directors: Ethan Coen, Joel Coen | Stars: Tommy Lee Jones, Javier Bardem, Josh Brolin, Woody Harrelson",directorList,castList);
-//        System.out.println("Director");
-//        for(String d:directorList){
-//            System.out.println(d);
-//        }
-//        System.out.println("cast");
-//        for(String d:castList){
-//            System.out.println(d);
-        }
+
+
 
 
     }
+
+
+}
 
 
